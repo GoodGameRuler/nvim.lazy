1c3fd1ea24ff1f1e49ee428b84cf534327bbe879
Udit <udit.samant@gmail.com>
2024-03-24 06:04:55 +0900

    Add sql lsp functionality

--- lua/GoodGameRuler/plugins/lsp/lspconfig.lua
+++ lua/GoodGameRuler/plugins/lsp/lspconfig.lua
@@ -123,6 +123,12 @@ return {
         --     on_attach = on_attach
         -- })
 
+        --PHP
+        lspconfig["sqlls"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach
+        })
+
         -- configure lua server (with special settings)
         lspconfig["lua_ls"].setup({
             capabilities = capabilities,
