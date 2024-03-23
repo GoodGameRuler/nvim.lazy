f37f5aeb3dec43354b22ec0ad74772ffcf916c7f
Udit <udit.samant@gmail.com>
2024-03-24 04:04:27 +0900

    Ensure lazygit loads by adding event

--- lua/GoodGameRuler/plugins/lazy-git.lua
+++ lua/GoodGameRuler/plugins/lazy-git.lua
@@ -1,5 +1,6 @@
 return {
     "kdheepak/lazygit.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     cmd = {
         "LazyGit",
         "LazyGitConfig",
@@ -7,13 +8,10 @@ return {
         "LazyGitFilter",
         "LazyGitFilterCurrentFile",
     },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-
+    dependencies = { "nvim-lua/plenary.nvim", },
     config = function ()
         -- set keymaps
+        require("lazygit")
         local keymap = vim.keymap -- for conciseness
 
         keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Git managment" })
