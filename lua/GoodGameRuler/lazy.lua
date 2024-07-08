84c70c2460f97e586135738e9022ebd7796e6882
Udit <udit.samant@gmail.com>
2024-07-09 03:49:18 +1000

    Add loading of different components to lazy

--- lua/GoodGameRuler/lazy.lua
+++ lua/GoodGameRuler/lazy.lua
@@ -1,33 +1,45 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy = require("lazy").setup({
-    {import = "GoodGameRuler.plugins"},
-    {import = "GoodGameRuler.plugins.lsp"},
-    {import = "GoodGameRuler.plugins.themes"}
+	{ import = "GoodGameRuler.plugins" },
+	{ import = "GoodGameRuler.plugins.autocomplete" },
+	{ import = "GoodGameRuler.plugins.file_management" },
+	{ import = "GoodGameRuler.plugins.formatting" },
+	{ import = "GoodGameRuler.plugins.git" },
+	{ import = "GoodGameRuler.plugins.linting" },
+	{ import = "GoodGameRuler.plugins.lsp" },
+	{ import = "GoodGameRuler.plugins.programming_utils" },
+	{ import = "GoodGameRuler.plugins.themes" },
+	{ import = "GoodGameRuler.plugins.ui" },
+	{ import = "GoodGameRuler.plugins.utils" },
 }, {
-    install  = {
-        colorscheme = {"everblush"},
-    },
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-    change_detection = {
-        notify = false,
-    },
+	install = {
+		colorscheme = { "everblush", "catppuccin" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
 
 vim.keymap.set("n", "<leader>lo", ":Lazy<CR>")
 vim.keymap.set("n", "<leader>li", ":Lazy install<CR>")
 vim.keymap.set("n", "<leader>ll", ":Lazy load all<CR>")
+
+vim.cmd.colorscheme("catppuccin")
