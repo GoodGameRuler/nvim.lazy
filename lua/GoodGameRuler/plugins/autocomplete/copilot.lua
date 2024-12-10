23763bb009b2558b824211cbcd0cdba05629f045
Udit Samant <udit.samant@safetyculture.io>
2024-12-10 16:12:15 +1100

    Add minor rebase

---
+++ lua/GoodGameRuler/plugins/autocomplete/copilot.lua
@@ -0,0 +1,27 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+				hide_during_completion = true,
+			},
+			filetypes = {
+				["*"] = false,
+				javascript = true,
+				go = true,
+				python = true,
+				sh = true,
+				yaml = true,
+				markdown = true,
+				gitcommit = true,
+				gitconfig = true,
+				sql = true,
+			},
+			copilot_node_command = "node", -- Node.js version must be > 18.x
+			server_opts_overrides = {},
+		})
+	end,
+}
