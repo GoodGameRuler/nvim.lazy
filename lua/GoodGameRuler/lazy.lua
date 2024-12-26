a1d489913a9fbc194c3a6176d92819be48c94a5b
Udit Samant <udit.samant@safetyculture.io>
2024-12-26 16:25:57 +1100

    Add copilot

--- lua/GoodGameRuler/lazy.lua
+++ lua/GoodGameRuler/lazy.lua
@@ -2,52 +2,51 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 ---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy = require("lazy").setup({
-	-- { import = "GoodGameRuler.plugins" },
-	{ import = "GoodGameRuler.plugins.autocomplete" },
-	{ import = "GoodGameRuler.plugins.file_management" },
-	{ import = "GoodGameRuler.plugins.notetaking" },
-	-- { import = "GoodGameRuler.plugins.git" },
-	{ import = "GoodGameRuler.plugins.lsp" },
-	{ import = "GoodGameRuler.plugins.programming_utils" },
-	{ import = "GoodGameRuler.plugins.themes" },
-	{ import = "GoodGameRuler.plugins.ui" },
-	{ import = "GoodGameRuler.plugins.utils" },
-	-- { import = "GoodGameRuler.plugins.format-linting" },
-	{ import = "GoodGameRuler.plugins.linting" },
-	{ import = "GoodGameRuler.plugins.formatting" },
+  -- { import = "GoodGameRuler.plugins" },
+  { import = "GoodGameRuler.plugins.autocomplete" },
+  { import = "GoodGameRuler.plugins.file_management" },
+  { import = "GoodGameRuler.plugins.notetaking" },
+  { import = "GoodGameRuler.plugins.git" },
+  { import = "GoodGameRuler.plugins.lsp" },
+  { import = "GoodGameRuler.plugins.debugging" },
+  { import = "GoodGameRuler.plugins.programming_utils" },
+  { import = "GoodGameRuler.plugins.themes" },
+  { import = "GoodGameRuler.plugins.ui" },
+  { import = "GoodGameRuler.plugins.utils" },
+  -- { import = "GoodGameRuler.plugins.format-linting" },
+  { import = "GoodGameRuler.plugins.linting" },
+  { import = "GoodGameRuler.plugins.formatting" },
 }, {
-	install = {
-		colorscheme = { "catppuccin", "tokyo-night" },
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-	{
-		rocks = {
-			enabled = true,
-			hererocks = true,
-		},
-	},
+  install = {
+    colorscheme = { "catppuccin", "tokyo-night" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+  {
+    rocks = {
+      enabled = true,
+      hererocks = true,
+    },
+  },
 })
 
 vim.keymap.set("n", "<leader>lo", ":Lazy<CR>")
 vim.keymap.set("n", "<leader>li", ":Lazy install<CR>")
 vim.keymap.set("n", "<leader>ll", ":Lazy load all<CR>")
-
-vim.cmd.colorscheme("catppuccin-macchiato")
--- lua/GoodGameRuler/plugins/autocomplete/copilot.lua
+++ lua/GoodGameRuler/plugins/autocomplete/copilot.lua
@@ -1,27 +1,31 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				auto_trigger = true,
-				hide_during_completion = true,
-			},
-			filetypes = {
-				["*"] = false,
-				javascript = true,
-				go = true,
-				python = true,
-				sh = true,
-				yaml = true,
-				markdown = true,
-				gitcommit = true,
-				gitconfig = true,
-				sql = true,
-			},
-			copilot_node_command = "node", -- Node.js version must be > 18.x
-			server_opts_overrides = {},
-		})
-	end,
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+        hide_during_completion = true,
+        keymap = {
+          accept = "<Right>",
+        }
+      },
+      filetypes = {
+        ["*"] = false,
+        javascript = true,
+        go = true,
+        golang = true,
+        python = true,
+        sh = true,
+        yaml = true,
+        markdown = true,
+        gitcommit = true,
+        gitconfig = true,
+        sql = true,
+      },
+      copilot_node_command = "node", -- Node.js version must be > 18.x
+      server_opts_overrides = {},
+    })
+  end,
 }
